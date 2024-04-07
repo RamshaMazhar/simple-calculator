c574d2524c76355c42b5d2f5bd1390b3fc41528b
ramsha mazhar <ramshamazhar56@gmail.com>
2024-04-08 00:32:51 +0500

    my simple CLI calculator

---
+++ index.ts
@@ -0,0 +1,35 @@
+
+  import inquirer from "inquirer";
+
+   //asking questions from user through inquirer
+   let answer= await inquirer.prompt ([
+    {message:"Enter first number",type: "number",name:"firstNumber"},
+    {message: "Enter second number",type: "number",name:"secondNumber"},
+    {message: "select one of the operators to perform operation",
+type:"list",
+name:"operator",
+choices: ["Addition","Subtrction","Multiplication","Division"],
+},
+   ]);
+
+//condional statements If-Else
+if(answer.operator ==="Addition"){
+    console.log(answer.firstNumber + answer.secondNumber 
+         );
+      }
+         else if (answer.operator === "Subtraction")
+            {console.log(answer.firstNumber - answer.secondNumber);
+
+         }
+         else if (answer.operator === "Multiplication")
+   {
+      console.log(answer.firstNumber * answer.secondNumber);
+
+         }
+         else if (answer.operator === "Division")
+            {console.log(answer.firstNumber / answer.secondNumber);
+
+         }
+         else{console.log("invalid input");
+
+         }
